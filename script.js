77f080617d9514dcdc6e03bb0d7dd1c71d608722
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-11-24 10:14:49 +0530

    add time elapsed till last refresh timer using js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-setTimeout(() => {
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'green';
-},1000)
+// setTimeout(() => {
+//     document.getElementsByTagName('body')[0].style.backgroundColor = 'green';
+// },1000)
+
+let i=0;
+document.getElementById('timer').innerHTML="Time elapsed since last refresh: "+i+" seconds"
+
+setInterval(() => {
+    i++
+    document.getElementById('timer').innerHTML="Time elapsed since last refresh: "+i+" seconds"
+}, 1000);
